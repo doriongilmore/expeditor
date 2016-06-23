145358f011baa3cab16949a4be7c79071ad82bf8
doriongilmore <erwan_laurent_56@hotmail.fr>
2016-06-23 14:33:59 +0200

    Update m_bdutilisateur

--- application/models/table/M_bdUtilisateur.php
+++ application/models/table/M_bdUtilisateur.php
@@ -49,7 +49,28 @@ class M_bdUtilisateur extends MY_Model{
     }
       
     public function update($data){
-         return parent::update($this->main_table, $data);   
+        
+//        $arrayData = [];
+//        if(!is_array($data))
+//            foreach ($data as $key => $value)
+//                $arrayData[$key] = $data->get($key) ;
+//        else
+//            $arrayData = $data;
+        
+        $arrayData = array(
+            'id_profil' => $data->get('id_profil'),
+            'prenom' => $data->get('prenom'),
+            'nom' => $data->get('nom')
+        );
+        
+        
+        $this->db->where('id_utilisateur', $data->get('id_utilisateur'));
+        $this->db->update('utilisateur', $arrayData);
+        $retour = ($this->db->affected_rows()>0)?true:false;
+        return $retour ;
+        
+        
+//         return parent::update($this->main_table, $data);   
     }
     
     public function getAllEmploye(){
